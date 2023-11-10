a532faccef50344c3d661d763952782df1fd11a6
Hyunjun KIM <hihj070914@icloud.com>
2023-11-10 15:24:15 +0900

    feat: db connection - 서버와 db를 typeorm을 통해 연결하였습니다.

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, '..', 'client'),
+        }),
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.POSTGRES_USER_ID,
+            password: process.env.POSTGRES_USER_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [],
+            synchronize: true,
         }),
     ],
     controllers: [AppController],
